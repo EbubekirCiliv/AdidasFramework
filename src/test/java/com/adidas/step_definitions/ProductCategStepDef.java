ea69798e335e4caebd33f684bee41f93dcda84f7
ebubekirc <72078134+EbubekirCiliv@users.noreply.github.com>
2021-10-14 01:58:28 +0200

    Adidas Framework has been created

---
+++ src/test/java/com/adidas/step_definitions/ProductCategStepDef.java
@@ -0,0 +1,10 @@
+package com.adidas.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProductCategStepDef {
+
+
+}
