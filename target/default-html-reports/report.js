35e7b13c6878a0b0a93f77f1afccb7e0bae0dfdc
ebubekirc <72078134+EbubekirCiliv@users.noreply.github.com>
2021-10-17 01:01:28 +0200

    Until title should be Sony vaio

---
+++ target/default-html-reports/report.js
@@ -0,0 +1,143 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/navigate.feature");
+formatter.feature({
+  "name": "Navigation Menu",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@navigate"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Navigating -- Laptop",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@navigate"
+    }
+  ]
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer on the Home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.NavigationMenuStepDef.the_customer_on_the_Home_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer click on Laptops",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.NavigationMenuStepDef.the_customer_click_on_Laptops()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer click on Sony vaio i5",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.NavigationMenuStepDef.the_customer_click_on_Sony_vaio_i5()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer click on Add to cart",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.NavigationMenuStepDef.the_customer_click_on_Add_to_cart()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer Accept pop up confirmation",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.NavigationMenuStepDef.the_customer_Accept_pop_up_confirmation()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the title should be Sony vaio i5",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.NavigationMenuStepDef.the_title_should_be_Sony_vaio_i5()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "skipped"
+});
+formatter.uri("file:src/test/resources/features/productcategories.feature");
+formatter.feature({
+  "name": "Product Categories Page",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Product Options Customer",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.before({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer navigate the Home page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.ProductCategStepDef.the_customer_navigate_the_Home_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "the customer should see following options",
+  "rows": [
+    {},
+    {},
+    {},
+    {}
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.step_definitions.ProductCategStepDef.the_customer_should_see_following_options(java.util.List\u003cjava.lang.String\u003e)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "skipped"
+});
+formatter.after({
+  "status": "skipped"
+});
+});
